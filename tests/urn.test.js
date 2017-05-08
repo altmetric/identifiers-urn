afb3eb465aa2a085344e6197f61793780baa7785
Jonathan Hernandez <jbilbo@gmail.com>
2017-05-08 10:57:24 +0200

    Implement URN extraction

---
+++ tests/urn.test.js
@@ -0,0 +1,16 @@
+/* globals test, expect */
+"use strict";
+
+const urn = require("../lib/urn");
+
+test("extracts the URNs from a string", () => {
+    var text = "En un pueblo italiano urn:1234:abc al pie de la montaña URN:foo:bar%23.\\";
+
+    expect(urn.extract(text)).toEqual(["urn:1234:abc", "URN:foo:bar%23."]);
+});
+
+test("only extracts URNs with word boundaries at the beginning", () => {
+    var text = "sideburn:mutton:chops";
+
+    expect(urn.extract(text)).toEqual([]);
+});
