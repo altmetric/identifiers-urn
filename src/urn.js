4bdfffe99d4f09a9148c783119ae0eb39b34ce54
Jonathan Hernandez <jbilbo@gmail.com>
2024-11-19 00:49:52 +0100

    Fix no-useless-escape eslint error
    error  Unnecessary escape character: \-
    error  Unnecessary escape character: \'

--- src/urn.js
+++ src/urn.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const URN_REGEX = /\burn:((?!urn:)[a-z0-9][a-z0-9\-]{1,31}):((?:[a-z0-9()+,-.:=@;$_!*\']|%(?:2[1-9a-f]|[3-6][0-9a-f]|7[0-9a-e]))+)/ig;
+const URN_REGEX = /\burn:((?!urn:)[a-z0-9][a-z0-9-]{1,31}):((?:[a-z0-9()+,-.:=@;$_!*']|%(?:2[1-9a-f]|[3-6][0-9a-f]|7[0-9a-e]))+)/ig;
 
 function scan(str) {
     return find_matches(str, function(matches) {
